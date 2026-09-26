80e288bb96c7042d3ffbf499117e792c88821f31
Le Duy Anh <lenduya@gmail.com>
2026-09-26 17:56:34 +0200

    refactor: swagger doc annotations

--- src/main/kotlin/io/github/sophon/employees/adapter/in/web/EmployeeRestController.kt
+++ src/main/kotlin/io/github/sophon/employees/adapter/in/web/EmployeeRestController.kt
@@ -6,6 +6,10 @@ import io.github.sophon.employees.application.port.`in`.DeleteEmployeeUseCase
 import io.github.sophon.employees.application.port.`in`.FindAllEmployeesUseCase
 import io.github.sophon.employees.application.port.`in`.FindEmployeeByIdUseCase
 import io.github.sophon.employees.application.port.`in`.UpdateEmployeeUseCase
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.constraints.Min
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -13,10 +17,12 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/employees")
+@Tag(name = "Employee REST API endpoints", description = "Operations related to employees")
 internal class EmployeeRestController(
     private val findAllEmployeesUseCase: FindAllEmployeesUseCase,
     private val findEmployeeByIdUseCase: FindEmployeeByIdUseCase,
@@ -26,20 +32,26 @@ internal class EmployeeRestController(
 ) {
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get all employees", description = "Get a list of all employees")
     fun findAll(): List<Employee> {
         val employeeList = findAllEmployeesUseCase()
         return employeeList
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get an employee", description = "Get an employee by ID")
     fun findById(
-        @PathVariable id: Long,
+        @PathVariable @Min(value = 1)
+        id: Long,
     ): Employee {
         val employee = findEmployeeByIdUseCase(id)
         return employee
     }
 
     @PostMapping
+    @Operation(summary = "Create an employee", description = "Create a new employee")
     fun createEmployee(
         @RequestBody employeeRequestDto: EmployeeRequestDto,
     ): Employee {
@@ -50,6 +62,7 @@ internal class EmployeeRestController(
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update an employee", description = "Update an employee of ID")
     fun updateEmployee(
         @PathVariable id: Long,
         @RequestBody employeeRequestDto: EmployeeRequestDto,
@@ -61,6 +74,7 @@ internal class EmployeeRestController(
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete an employee", description = "Delete an employee by ID")
     fun deleteEmployee(
         @PathVariable id: Long,
     ) {
